97adea44bdcc3a8f96022734823b59b26a744c47
Brick Stine <brickstine04@gmail.com>
2024-12-14 13:12:13 -0600

    Added the test file structure, wrote the basics for product controller tests and cleaned up the file structure

--- userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login, register } = require("../controllers/authController");
-
-// User authentication routes
-router.post("/login", login);
-router.post("/register", register); // For user registration
-
-module.exports = router;
